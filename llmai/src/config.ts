480d7e89b66aa28c6e7d2b7d595a9d52c2c38030
Daniel James <daniel@daniels-mbp.lan>
2024-07-31 07:18:14 -0600

    compile succesfully without config.json by default

--- llmai/src/config.ts
+++ llmai/src/config.ts
@@ -1,11 +1,21 @@
-// comment out this line if you'd prefer to use env variables
-import config from "./config.json";
+const maybeConfig: { [key: string]: string } = {}
+
+// uncomment this line if you'd prefer not to use env variables
+// import config from "./config.json";
+
+// Example format for config.json
 // {
 //     "ANTHROPIC_API_KEY": "...",
 //     "OPENAI_API_KEY": "...",
 //     "TOGETHER_API_KEY": "..."
 // }
 
-export const ANTHROPIC_API_KEY = process.env["ANTHROPIC_API_KEY"] || config["ANTHROPIC_API_KEY"];
-export const OPENAI_API_KEY = process.env["OPENAI_API_KEY"] || config["OPENAI_API_KEY"];
-export const TOGETHER_API_KEY = process.env["TOGETHER_API_KEY"] || config["TOGETHER_API_KEY"];
+// @ts-ignore
+if (typeof config != 'undefined') {
+    // @ts-ignore
+    Object.assign(maybeConfig, config);
+}
+
+export const ANTHROPIC_API_KEY = process.env["ANTHROPIC_API_KEY"] || maybeConfig["ANTHROPIC_API_KEY"];
+export const OPENAI_API_KEY = process.env["OPENAI_API_KEY"] || maybeConfig["OPENAI_API_KEY"];
+export const TOGETHER_API_KEY = process.env["TOGETHER_API_KEY"] || maybeConfig["TOGETHER_API_KEY"];
